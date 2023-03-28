75c8acbe942854506d9d34e5f1047b36db5196bd
eklempere <119396267+eklempere@users.noreply.github.com>
2023-03-28 08:47:19 +0300

    pievienoju nepieciesamas funkcijas

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const plusins = document.querySelector('.js-plus');
 const inputins = document.querySelector('.js-input');
 
-console.log(plusins)
+const Add = (cipars1, cipars2) => {
+    return cipars1 + cipars2
+}
+
+constSubstract = (cipars1, cipars2) => {
+    return cipars1 - cipars2
+}
+
+const Multiply = (cipars1, cipars2) => {
+    return cipars1 * cipars2
+}
+
+const Divide = (cipars1, cipars2) => {
+    return cipars1 / cipars2
+}
+
+plusins.addEventListener('click', () => (
+    console.log("Mani nospiedumi")
+))
+
+console.log(Add(2,3))
+
 console.log(inputins)
